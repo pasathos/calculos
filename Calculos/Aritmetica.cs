e74c6fe343429ccddd2cd1cc995afc7815018c90
Paulo Alexandre dos Santos <17698507+pasathos@users.noreply.github.com>
2025-01-24 20:30:32 +0000

    Adicionar método Subtrair()

--- Calculos/Aritmetica.cs
+++ Calculos/Aritmetica.cs
@@ -10,11 +10,15 @@ namespace Calculos
     /// Implementa operações aritméticas.
     /// </summary>
     internal class Aritmetica
+    {
+        public int Somar(int x, int y)
         {
-            public int Somar(int x, int y)
-            {
-                return x + y;
-            }
+            return x + y;
         }
+        public int Subtrair(int x, int y)
+        {
+            return x - y;
+        }
+    }
 
 }
